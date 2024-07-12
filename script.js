649e49b1c6ea47ca524a991b5b12fff18004153b
Desmond <142880728+Desmond23Y@users.noreply.github.com>
2024-07-12 11:02:13 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ window.addEventListener('load', () => {
     // Add a delay of 15 seconds (15000 milliseconds)
     setTimeout(() => {
         document.body.classList.add('loaded');
-    }, 15000);
+    }, 3000);
 });
